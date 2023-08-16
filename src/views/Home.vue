38f42ba375220d21f8c8d9d615c30b336a7166da
妖雨纯 <876451336@qq.com>
2023-08-16 17:30:44 +0800

    ✨ feat(Home): 回填首页密钥

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,18 @@
 <script lang="tsx">
 import { defineComponent, computed } from 'vue'
-import { useNow, useDateFormat } from '@vueuse/core'
-import { compute, type INameUI } from '@/utils/utils-remix'
+import { useNow, useDateFormat, useClipboard } from '@vueuse/core'
+import { compute, sompute, type INameUI } from '@/utils/utils-remix'
 import { useUser } from '@/store/user'
 import { useState } from '@/hooks/hook-state'
 import { useCurrent } from '@/locale/instance'
 import { useResize } from '@/hooks/hook-resize'
-import { divineCols } from '@/utils/utils-common'
+import { divineCols, divineHandler } from '@/utils/utils-common'
+import { createNotice } from '@/utils/utils-naive'
 
 export default defineComponent({
     name: 'Home',
     setup(props) {
+        const { copy, isSupported } = useClipboard()
         const { width } = useResize()
         const { t, tm } = useCurrent()
         const { state } = useState({ loading: false })
@@ -33,6 +35,22 @@ export default defineComponent({
             }
         }))
 
+        /**复制事件**/
+        async function onSupporter(value: string) {
+            return await divineHandler(isSupported.value, async () => {
+                try {
+                    await copy(value)
+                    return await createNotice({ title: t('common.copy.notice') })
+                } catch (e) {
+                    return await createNotice({ type: 'error', title: t('common.copy.fail') })
+                }
+            }).then(result => {
+                return divineHandler(!result, () => {
+                    return createNotice({ type: 'error', title: t('common.copy.supported') })
+                })
+            })
+        }
+
         return () => (
             <common-container max-width="1680px" react-style={{ padding: '64px 32px', margin: '20px 0' }}>
                 <n-h1 strong>
@@ -52,10 +70,32 @@ export default defineComponent({
                         <n-blockquote style={{ margin: '0 0 30px' }}>{client.value.document}</n-blockquote>
                         <n-form ref="formRef" size="large" label-placement="top">
                             <n-form-item label="API Key">
-                                <n-input value={user.appKey} placeholder="API Key" readonly class="el-customize" />
+                                <n-input-group>
+                                    <n-input value={user.appKey} type="text" placeholder="API Key" readonly />
+                                    <n-button
+                                        focusable={false}
+                                        style={{ padding: '0 12px' }}
+                                        v-slots={{ icon: () => sompute('CopyRound', { size: '18px', color: 'var(--n-icon-color)' }) }}
+                                        onClick={(e: Event) => onSupporter(user.appKey)}
+                                    ></n-button>
+                                </n-input-group>
                             </n-form-item>
                             <n-form-item label="API Secret" show-feedback={false}>
-                                <n-input value={user.appSecret} type="password" placeholder="API Secret" disabled />
+                                <n-input-group>
+                                    <n-input
+                                        value={user.appSecret}
+                                        type="password"
+                                        show-password-on="click"
+                                        placeholder="API Secret"
+                                        readonly
+                                    />
+                                    <n-button
+                                        focusable={false}
+                                        style={{ padding: '0 12px' }}
+                                        v-slots={{ icon: () => sompute('CopyRound', { size: '18px', color: 'var(--n-icon-color)' }) }}
+                                        onClick={(e: Event) => onSupporter(user.appSecret)}
+                                    ></n-button>
+                                </n-input-group>
                             </n-form-item>
                         </n-form>
                     </div>
@@ -110,6 +150,9 @@ export default defineComponent({
         display: flex;
         flex-direction: column;
         overflow: hidden;
+        .n-input {
+            background-color: var(--n-color-disabled);
+        }
     }
     &__approve {
         width: 380px;
