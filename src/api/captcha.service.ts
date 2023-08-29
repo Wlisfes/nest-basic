104f4f1a1f1895b8e638b0f4332c646e8012515c
妖雨纯 <876451336@qq.com>
2023-08-29 23:04:58 +0800

    ✨ feat(人机验证): 新增api相关接口

---
+++ src/api/captcha.service.ts
@@ -0,0 +1,39 @@
+import { request } from '@/utils/utils-request'
+import type { Result, IColumn, Notice } from '@/interface/common.resolver'
+import type { ServiceCaptcha } from '@/interface/captcha.resolver'
+
+/**创建验证应用**/
+export function httpCreateCaptchaService(data: { name: string }) {
+    return request<Notice>({
+        url: `/api-basic/captcha/app/create`,
+        method: 'POST',
+        data
+    })
+}
+
+/**编辑授权地址**/
+export function httpUpdateCaptchaBucket(data: { appId: number; bucket: Array<string>; ip: Array<string> }) {
+    return request<Notice>({
+        url: `/api-basic/captcha/app/update/bucket`,
+        method: 'POST',
+        data
+    })
+}
+
+/**验证应用列表**/
+export function httpColumnCaptchaService(params: Pick<IColumn, 'page' | 'size'>) {
+    return request<Result<ServiceCaptcha>>({
+        url: `/api-basic/captcha/app/column`,
+        method: 'GET',
+        params
+    })
+}
+
+/**验证应用信息**/
+export function httpBasicCaptchaService(params: { appId: number }) {
+    return request<ServiceCaptcha>({
+        url: `/api-basic/captcha/app/basic`,
+        method: 'GET',
+        params
+    })
+}
--- src/interface/captcha.resolver.ts
+++ src/interface/captcha.resolver.ts
@@ -4,10 +4,11 @@ import type { IUser } from '@/interface/user.resolver'
 export interface ServiceCaptcha extends CommonResolver {
     appId: number
     name: string
-    appKey: string
     appSecret: string
+    comment: string
     bucket: Array<string>
     ip: Array<string>
     user: IUser
-    status: 'inactivated' | 'activated' | 'disable' | 'delete'
+    visible: 'hide' | 'show'
+    status: 'activated' | 'disable' | 'delete'
 }
