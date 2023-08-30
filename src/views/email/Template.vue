3d2b2f4409f8abe1d611e1a2fd387c3e71474b3d
妖雨纯 <876451336@qq.com>
2023-08-30 23:27:03 +0800

    ✨ feat(Base): 新增拖拽实例实现

--- src/views/email/Template.vue
+++ src/views/email/Template.vue
@@ -1,12 +1,23 @@
 <script lang="tsx">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import { useResize } from '@/hooks/hook-resize'
+import { useState } from '@/hooks/hook-state'
 import { whereProperter } from '@/utils/utils-layout'
+import { vDraggable, VueDraggable } from 'vue-draggable-plus'
 
 export default defineComponent({
     name: 'Template',
+    directives: { vDraggable },
+    components: { VueDraggable },
     setup() {
         const { mobile } = useResize()
+
+        const dataColumn = ref([
+            { name: 'Image', uid: 1693405215423 },
+            { name: 'Button', uid: 1693405215424 },
+            { name: 'Layout', uid: 1693405215425 },
+            { name: 'Text', uid: 1693405215426 }
+        ])
 
         return () => (
             <common-container
@@ -14,8 +25,28 @@ export default defineComponent({
                 content-style={whereProperter(mobile.value, { padding: '0 20px' }, { padding: '0 40px' })}
                 request-style={whereProperter(mobile.value, { padding: '40px 20px 20px' }, { padding: '60px 40px 30px' })}
                 request={<common-header vertical={mobile.value} title="发送模板"></common-header>}
-            ></common-container>
+            >
+                <n-grid x-gap={20} y-gap={20} cols={3} style={{ flex: 1 }}>
+                    <n-grid-item span={1}></n-grid-item>
+                    <n-grid-item span={2}>
+                        <vue-draggable v-model={dataColumn.value} ghostClass="ghost" animation={150}>
+                            {dataColumn.value.map(item => (
+                                <n-card key={item.uid} embedded>
+                                    {item.name}
+                                </n-card>
+                            ))}
+                        </vue-draggable>
+                    </n-grid-item>
+                </n-grid>
+            </common-container>
         )
     }
 })
 </script>
+
+<style scoped>
+.ghost {
+    opacity: 0.5;
+    background: #c8ebfb;
+}
+</style>
