acaa05af86e5c7cd2b8a917007d8f0ed100b2b52
妖雨纯 <876451336@qq.com>
2023-09-03 17:20:01 +0800

    ✨ feat(邮件 - 发送模板): 完善text编辑器配置

--- src/views/mailer/components/element-text.vue
+++ src/views/mailer/components/element-text.vue
@@ -2,7 +2,6 @@
 import { defineComponent, ref, computed, watch, onMounted, type PropType } from 'vue'
 import { useVModels } from '@vueuse/core'
 import { type NestOption } from '@/utils/utils-mailer'
-// import InlineEditor from '@ckeditor/ckeditor5-editor-inline'
 
 export default defineComponent({
     name: 'ElementText',
@@ -15,19 +14,47 @@ export default defineComponent({
         // const instance = ref<InlineEditor>()
 
         onMounted(() => {
-            // InlineEditor.create(element.value as HTMLElement, {
-            //     // initialData: node.value.content,
-            //     language: 'zh',
-            //     placeholder: '请输入'
-            // }).then(ckeditor => {
-            //     ckeditor.model.document.on('change', e => {
-            //         node.value.content = ckeditor.getData()
-            //     })
-            //     return (instance.value = ckeditor)
-            // })
+            ;(window as any).InlineEditor.create(element.value as HTMLElement, {
+                initialData: node.value.content,
+                language: 'en',
+                placeholder: '请输入',
+                toolbar: {
+                    shouldNotGroupWhenFull: true
+                    // items: [
+                    //     'heading',
+                    //     '|',
+                    //     'fontFamily',
+                    //     'fontSize',
+                    //     'fontColor',
+                    //     'fontBackgroundColor',
+                    //     'alignment',
+                    //     'indent',
+                    //     'outdent',
+                    //     '|',
+                    //     'bold',
+                    //     'italic',
+                    //     'link',
+                    //     'underline',
+                    //     'strikethrough',
+                    //     'horizontalLine',
+                    //     'bulletedList',
+                    //     'numberedList',
+                    //     'selectAll',
+                    //     '|',
+                    //     'undo',
+                    //     'redo'
+                    // ]
+                }
+            }).then((ckeditor: any) => {
+                ckeditor.model.document.on('change', (e: any) => {
+                    node.value.content = ckeditor.getData()
+                })
+            })
         })
 
-        return () => <div>1111111111</div>
+        return () => <div class="element-text" ref={element}></div>
     }
 })
 </script>
+
+<style lang="scss" scoped></style>
