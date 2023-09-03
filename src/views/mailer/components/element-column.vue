8f3626430e79449bd6f661455d3d17a93c1c970e
妖雨纯 <876451336@qq.com>
2023-09-03 17:48:30 +0800

    ✨ feat(邮件 - 发送模板): 新增element组件实例

---
+++ src/views/mailer/components/element-column.vue
@@ -0,0 +1,13 @@
+<script lang="tsx">
+import { defineComponent, ref, onMounted, type PropType } from 'vue'
+import { useVModels } from '@vueuse/core'
+import { type NestOption } from '@/utils/utils-mailer'
+
+export default defineComponent({
+    name: 'ElementColumn',
+
+    setup() {
+        return () => <div></div>
+    }
+})
+</script>
