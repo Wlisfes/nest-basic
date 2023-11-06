8ee2e0df5998d1b1d7425e3950531dba46ae0624
妖雨纯 <876451336@qq.com>
2023-11-06 22:16:55 +0800

    ✨ feat(captchar - 应用服务): 完善tag类型映射

--- src/views/captchar/common/captchar-suppor.vue
+++ src/views/captchar/common/captchar-suppor.vue
@@ -13,9 +13,14 @@ export default defineComponent({
         const { CAPTCHAR_SUPPOR } = useLocale()
 
         return () => (
-            <n-tag round bordered={false} style={{ marginBottom: 'auto', padding: '0 10px 0 15px' }}>
+            <n-tag
+                round
+                bordered={false}
+                type={divineColumn(CAPTCHAR_SUPPOR.value, props.value, { key: 'type' })}
+                style={{ marginBottom: 'auto', padding: '0 10px 0 15px' }}
+            >
                 {{
-                    default: () => <span>{divineColumn(CAPTCHAR_SUPPOR.value, props.value, { key: 'recover' })}</span>,
+                    default: () => <span>{divineColumn(CAPTCHAR_SUPPOR.value, props.value, { key: 'label' })}</span>,
                     icon: (e: unknown) => {
                         return props.value === 'activated' ? (
                             <n-icon color="var(--n-text-color)" depth={1} size={20} component={compute('EnableRound')} />
