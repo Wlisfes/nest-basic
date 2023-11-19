191e5d4abdee97e9801e72b12861b2d290f73db1
妖雨纯 <876451336@qq.com>
2023-11-19 22:51:10 +0800

    ✨ feat(api): 新增注册接口

--- src/api/common.service.ts
+++ src/api/common.service.ts
@@ -1,10 +1,19 @@
 import { request } from '@/utils/utils-request'
-import type { Result, IColumn } from '@/interface/static.resolver'
+import type { Result, IColumn, Notice } from '@/interface/static.resolver'
 import type { ICustomer } from '@/interface/common.resolver'
+
+/**注册**/
+export function httpCommonCustomerRegister(data: { nickname: string; password: string; mobile: string; code: string }) {
+    return request<Notice>({
+        url: `/common/customer/register`,
+        method: 'POST',
+        data
+    })
+}
 
 /**登录**/
 export function httpCommonCustomerAuthorize(data: { mobile: string; password: string; token: string; session: string }) {
-    return request<{ token: string; refresh: string; expire: number; message: string }>({
+    return request<Notice & { token: string; session: string; expire: number }>({
         url: `/common/customer/authorize`,
         method: 'POST',
         data
